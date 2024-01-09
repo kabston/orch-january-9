4c2baf80c3dc9db3ef413f9f4eed1ae10fae40cc
kabston <144282215+kabston@users.noreply.github.com>
2024-01-09 16:14:47 -0500

    Add files via upload

---
+++ python-scripts/script-1.py
@@ -0,0 +1,4 @@
+import datetime;
+
+current_time = datetime.datetime.now()
+print("Script 1 says the current time is ", current_time)
---
+++ python-scripts/script-2.py
@@ -0,0 +1,4 @@
+import datetime;
+
+current_time = datetime.datetime.now()
+print("Script 2 says the current time is ", current_time)
